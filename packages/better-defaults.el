d3a4c26261957bea922c343607d356a88a334be1
Jeremy Bi <bixuanxbi@gmail.com>
2014-03-15 21:55:58 +0800

    dired config

--- packages/better-defaults.el
+++ packages/better-defaults.el
@@ -140,20 +140,6 @@
 ;; keep in mind known issues with zsh - see emacs wiki
 (setq tramp-default-method "ssh")
 
-;; dired - reuse current buffer by pressing 'a'
-(put 'dired-find-alternate-file 'disabled nil)
-
-;; always delete and copy recursively
-(setq dired-recursive-deletes 'always)
-(setq dired-recursive-copies 'always)
-
-;; if there is a dired buffer displayed in the next window, use its
-;; current subdir, instead of the current subdir of this dired buffer
-(setq dired-dwim-target t)
-
-;; enable some really cool extensions like C-x C-j(dired-jump)
-(require 'dired-x)
-
 ;; Ido-related config
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
