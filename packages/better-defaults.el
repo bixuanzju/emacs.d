56f79a462ffa3caba97025779bc9e58e255c24d1
Jeremy Bi <bixuanxbi@gmail.com>
2014-03-22 18:44:30 +0800

    minor change

--- packages/better-defaults.el
+++ packages/better-defaults.el
@@ -100,11 +100,11 @@
 (setq blink-matching-paren nil)
 
 ;; meaningful names for buffers with the same name
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-(setq uniquify-separator "/")
-(setq uniquify-after-kill-buffer-p t)    ; rename after killing uniquified
-(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+;; (require 'uniquify)
+;; (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+;; (setq uniquify-separator "/")
+;; (setq uniquify-after-kill-buffer-p t)
+;; (setq uniquify-ignore-buffers-re "^\\*")
 
 ;; Nic says eval-expression-print-level needs to be set to nil (turned off) so
 ;; that you can always see what's happening.
