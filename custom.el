3bd87dbd7465c44fdb0302f81aaf98f7a44a354f
Jeremy Bi <bixuanxbi@gmail.com>
2014-06-16 13:49:44 +0800

    enable ghc-mod

--- custom.el
+++ custom.el
@@ -4,6 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(custom-safe-themes
+   (quote
+    ("025354235e98db5e7fd9c1a74622ff53ad31b7bde537d290ff68d85665213d85" "6fe6ab4abe97a4f13533e47ae59fbba7f2919583f9162b440dd06707b01f7794" default)))
  '(ebib-preload-bib-files (quote ("~/research/refs.bib")))
  '(eyebrowse-keymap-prefix (kbd "C-c C-q"))
  '(history-length 1000)
@@ -11,8 +14,6 @@
  '(max-lisp-eval-depth 2000)
  '(paradox-automatically-star t)
  '(set-mark-command-repeat-pop t)
- ;; '(shell-pop-shell-type (quote ("eshell" "*eshell*" (lambda nil (eshell)))))
- ;; '(shell-pop-universal-key "C-c \"")
  '(shift-select-mode nil)
  '(split-height-threshold nil)
  '(split-width-threshold 130)
@@ -25,5 +26,6 @@
  '(cursor ((t (:background "gray78" :foreground "#DCDCCC"))))
  '(flyspell-duplicate ((t nil)))
  '(flyspell-incorrect ((t (:inherit nil :underline (:color "yellow" :style wave)))))
+ '(persp-selected-face ((t (:inherit nil :foreground "yellow"))))
  '(shm-current-face ((t (:background "#2B2B2B"))))
  '(sr-active-path-face ((t (:weight bold :height 180)))))
