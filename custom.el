d8e0b14046da113812f02413dea1d3aa62dc8883
Jeremy Bi <bixuanxbi@gmail.com>
2014-06-29 10:48:46 +0800

    cleanup

--- custom.el
+++ custom.el
@@ -7,8 +7,6 @@
  '(custom-safe-themes
    (quote
     ("025354235e98db5e7fd9c1a74622ff53ad31b7bde537d290ff68d85665213d85" "6fe6ab4abe97a4f13533e47ae59fbba7f2919583f9162b440dd06707b01f7794" default)))
- '(ebib-preload-bib-files (quote ("~/research/refs.bib")))
- '(eyebrowse-keymap-prefix (kbd "C-c C-q"))
  '(history-length 1000)
  '(indicate-empty-lines t)
  '(max-lisp-eval-depth 2000)
