f18380f69de6d207d64c3226a16e8b757969aa7c
Jeremy Bi <bixuanxbi@gmail.com>
2014-10-27 09:13:29 +0800

    add hindent

--- init.el
+++ init.el
@@ -1,7 +1,3 @@
-;; Initialize cask to get the correct version of org-mode
-(require 'cask "/usr/local/share/emacs/site-lisp/cask.el")
-(cask-initialize)
-
 ;; Load customization
 ;; Keep emacs custom-settings in separate file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
