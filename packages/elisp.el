285aed4325f0e3a2000773afb12db15b1e33f3ac
Jeremy Bi <bixuanxbi@gmail.com>
2015-01-12 08:03:27 +0800

    Add mwim

--- packages/elisp.el
+++ packages/elisp.el
@@ -55,29 +55,6 @@ Passes ARG to command `kill-whole-line' when provided."
   (kill-whole-line arg)
   (back-to-indentation))
 
-(defun prelude-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
-  (interactive "^p")
-  (setq arg (or arg 1))
-
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-
 (defun prelude-top-join-line ()
   "Join the current line with the line beneath it."
   (interactive)
@@ -205,6 +182,16 @@ a region, all lines that region covers will be duplicated."
         (--remove (eql (current-buffer) it)))
     #'kill-buffer)
   (message "Done"))
+
+(defun my/forward-line-by-many ()
+  "Move line forward by multiple times."
+  (interactive)
+  (forward-line 4))
+
+(defun my/backward-line-by-many ()
+  "Move line backward by multiple times."
+  (interactive)
+  (forward-line -4))
 
 (provide 'elisp)
 
